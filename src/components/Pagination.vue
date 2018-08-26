6149fe63eb85098fd290f844d412ccaa61286dcc
Maja Markovic <markovicm9000@gmail.com>
2018-08-26 21:49:11 +0200

    search_i_paginacija

---
+++ src/components/Pagination.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <nav aria-label="Page navigation example" >
+            <ul class="pagination">
+                <li 
+                    class="page-item" 
+                     :class="{ 'active': numOfPage === page }"
+                    v-for="page in numberOfPages" :key="page">
+                    <a class="page-link" @click="selectPage(page)" >{{ page }}</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    props: {
+        numOfPage: {
+            type: Number
+        },
+
+        numberOfPages: {
+            type: Number
+        }
+    },
+
+    methods: {
+        selectPage(page){
+            this.$emit('selectPage', page)
+        }
+    }
+}
+</script>
+
--- src/components/TriviaApp.vue
+++ src/components/TriviaApp.vue
@@ -4,9 +4,14 @@
             <h2>Categories:</h2>
             <ul class="list-group list-group-flush" v-for="category in triviaCategory" :key="category.id">
                 <li class="list-group-item" @click="selectCategory">{{ category.title }}</li>
+               
             </ul><br>
+             <form class="form-inline">
+                <i class="fa fa-search" aria-hidden="true"></i>
+                <input v-model="searchedTerm" class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search">
+            </form><br>
             <h3>Questions:</h3><br>
-            <div class="card" v-for="trivia in trivias" :key="trivia.id" @click="toggle(trivia.id)">
+            <div class="card" v-for="trivia in paginationVisibleTrivia" :key="trivia.id" @click="toggle(trivia.id)">
                 <div class="card-header">
                     <h5 class="mb-0">{{ trivia.question }}</h5>
                 </div>
@@ -14,6 +19,8 @@
                     <div class="card-body">{{ trivia.answer }}</div>
                 </div>
             </div>
+            
+            <Pagination :numOfPage="numOfPage" :numberOfPages="numberOfPages" @selectPage="selectPage" />
         </div>
     </div>
 </template>
@@ -21,11 +28,18 @@
 <script>
 import { store } from './../store'
 import { mapGetters, mapActions } from 'vuex'
+import Pagination from './Pagination'
 
 export default {
+    components: {
+        Pagination
+    },
+    
     data(){
         return {
-            selectedTrivias: []
+            selectedTrivias: [],
+            searchedTerm: '',
+            numOfPage: 1
         }
     },
     computed: {
@@ -34,6 +48,23 @@ export default {
             triviaCategory: 'getTriviaCategories'
             
         }),
+
+        filteredTrivias(){
+            return this.trivias.filter(
+                trivia => trivia.question.toLowerCase().indexOf(this.searchedTerm.toLowerCase()) > -1)
+        },
+
+        paginationVisibleTrivia(){
+            let bottom = (this.numOfPage - 1) * 5
+            let top = bottom + 5
+            return this.filteredTrivias.filter((trivia, index) => {
+                return index > bottom && index <= top
+            })
+        },
+
+        numberOfPages(){
+            return Math.ceil(this.filteredTrivias.length / 5)
+        }
 
     },
 
@@ -56,6 +87,10 @@ export default {
 
         selectCategory(event){
             this.fetchTrivia(event.target.value)
+        },
+
+        selectPage(page){
+            return this.numOfPage = page
         }
 
     },
